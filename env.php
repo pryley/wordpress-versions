a8f9cce42dd51a406650b5ce8bd7f60dffaaf4ea
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-11-16 19:12:14 +0100

    misc

--- env.php
+++ env.php
@@ -4,12 +4,12 @@
 define('WP_ENV', 'development');
 
 define('ENVIRONMENTS', serialize([
-	// 'production'  => 'http://geminilabs.io',
-	// 'staging'     => 'http://wordpress.geminilabs.io',
-	'development' => 'http://wordpress.dev',
+	// 'production'  => 'https://example.com',
+	// 'staging'     => 'https://staging.example.com',
+	'development' => 'http://wordpress.test',
 ]));
 
-define('DB_NAME',     'wordpress_4.6');
+define('DB_NAME',     'wordpress_6.8');
 define('DB_USER',     'dev');
 define('DB_PASSWORD', 'dev');
 define('DB_HOST',     'localhost');
@@ -18,15 +18,18 @@ $table_prefix = 'gl_';
 
 // https://api.wordpress.org/secret-key/1.1/salt/
 // You can change these at any point in time to invalidate all existing cookies.
-define('AUTH_KEY',         'oC-R*)l}NC#?7 F7=(q&7SEP,1*{+ipPu`R%tG2e5cW-_n]:A.S+|9CL|(SgtUgz');
-define('SECURE_AUTH_KEY',  '0`}H){0vE.p&MFF05Vaz;koLRnGcRF:,7(fM|NpB3[0iw+3Sz=j5!Ly&7 %OV|Wr');
-define('LOGGED_IN_KEY',    'N:}8(pf4yS%k#17/l%&q9X;ti70?T,|P}BW3K&FDeGp.{ _V4=okb(Xrg-0,6)6]');
-define('NONCE_KEY',        '9aL6z)TF4%-|p+-<Xn^lQ!B(|w9Bq]hQ:1Lt#(=hvQk`d:[-A*=Sq76mGX9i)bY3');
-define('AUTH_SALT',        '?W,8t-)bP/oF$/wyhLd:gFR6~I:hA%!*tAQT}Ao},TCovm=]lCgti/A3^K?Pkfja');
-define('SECURE_AUTH_SALT', '1pQTF !,dszQp`S}ai~n/BIGZ&~.rP`h<hFBe  |(h7d>3@gju:<RikPxw8Yxc&>');
-define('LOGGED_IN_SALT',   '`YU5dm<:/QcNP74$Xrpp^Y%-szo2WMsiFWxZ[y;<D2Y.TdMQn/f/Y,2!aSr(HvX#');
-define('NONCE_SALT',       '2Q^x7+Ph>7A^t1 G(=gG7yLni):TQJ7I0=]k=X+*RS|Dv=dh!zx]~]n:2{AF=tj(');
+define('AUTH_KEY',         'F$G77*<-F2EYo&V%#ElAPZ`a/#tDHF&%{G$K6rhdTmxzCs[ND+)q v{|!S)#nm=h');
+define('SECURE_AUTH_KEY',  'wXf:e%+f+xZj1`0G!,1ZNOXLRhXo~MQrxJbK4 GOtB8C9ZMDD:`h9e?,8;1J<Tn.');
+define('LOGGED_IN_KEY',    'P}%T@P_/XA$;PVWU0/F+6+Z:FvBP%jxBAgVjp|u,|_,Kr9qr;2kVw=e@#bKi^Y`#');
+define('NONCE_KEY',        'CYpU?3odt`5ihluSRJV>cPfU%&Xf<a<Fe<<s034;Ed<Tjb89J2~2++}|+-X|`y20');
+define('AUTH_SALT',        'G)t3dMru]|Z8~yx~S/;#nc[+9<7^7h&Pokco@9R(n`AIFaf05Ji!NItc=5+`3A[-');
+define('SECURE_AUTH_SALT', 'X{:+q8aDt2[5X=-9P+w;yeD*`jo-xkgGV4T A+G,D-%|z/2!bO`$,9g*qxEkR)v.');
+define('LOGGED_IN_SALT',   'U@JW<Danr1qY@^!XfsR^D4IJ>;~Uzwm:N*n2lamA.hYY-x1@F|SDY-]-taJ!H9#,');
+define('NONCE_SALT',       'ZeX-<E<eyxZN+oG{%p#s^Z%pY+DIazS2unHmF7&r%8.N$1~/)GQGGsvHuZ$Vp*}K');
 
+define('DISALLOW_FILE_EDIT', false);
 define('SAVEQUERIES', true);
 define('SCRIPT_DEBUG', true);
 define('WP_DEBUG', true);
+// define('MAILTRAP_USERNAME', 'c4c8a865fd8857');
+// define('MAILTRAP_PASSWORD', 'a1796be3fb3546');
--- switch.sh
+++ switch.sh
@@ -8,36 +8,31 @@ GREY=`tput setaf 8`
 RED=`tput setaf 9`
 GREEN=`tput setaf 10`
 
-version() {
-	printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' '\n' | head -n 4)
-}
+WP_MIN_VERSION="6.0"
+WP_MAX_VERSION="$(curl -s https://api.wordpress.org/core/version-check/1.7/ | jq -r '.offers[0].version' | cut -d. -f1,2)"
+WP_PATH="--path=public/wp"
+WP_PREVIOUS_VERSION="$(wp core version $WP_PATH)"
 
 check_errors() {
 	ERRORS=()
-
-	# Check that mysql is installed
+	if ! which jq > /dev/null; then
+		ERRORS=("${ERRORS[@]}" "==>${WHITE} jq    ${GREY}${UNDERLINE}https://jqlang.github.io/jq/download/${NORMAL}")
+	fi
 	if ! which mysql > /dev/null; then
 		ERRORS=("${ERRORS[@]}" "==>${WHITE} mysql ${GREY}${UNDERLINE}https://dev.mysql.com/downloads/mysql/${NORMAL}")
 	fi
-
-	# Check that perl is installed
 	if ! which perl > /dev/null; then
 		ERRORS=("${ERRORS[@]}" "==>${WHITE} perl  ${GREY}${UNDERLINE}https://www.perl.org/get.html${NORMAL}")
 	fi
-
-	# Check that wp-cli is installed
 	if ! which wp > /dev/null; then
 		ERRORS=("${ERRORS[@]}" "==>${WHITE} wp    ${GREY}${UNDERLINE}https://wp-cli.org/#installing${NORMAL}")
 	fi
-
 	if [ ${#ERRORS[@]} -gt 0 ]; then
 		echo "${RED}${BOLD}Error: ${NORMAL}The following commands were not found:"
-
 		for error in "${ERRORS[@]}"
 		do
 			echo $"${error}"
 		done
-
 		exit 1
 	fi
 }
@@ -53,23 +48,20 @@ download() {
 switch_wp() {
 	if ! [ -d $WP_CORE_DIR ]; then
 		mkdir -p $WP_CORE_DIR
-		wp core download --version=$WP_VERSION --path=$WP_CORE_DIR
+		wp core download --version=$WP_NEW_VERSION --path=$WP_CORE_DIR
 	fi
-
-	echo "${BOLD}${GREEN}Switching to WordPress $WP_VERSION${NORMAL}"
-
+	echo "${BOLD}${GREEN}Switching to WordPress $WP_NEW_VERSION${NORMAL}"
 	rm -rf $DIR/public/wp;
 	cp -R $WP_CORE_DIR $DIR/public/wp
-
 	DB_NAME=$(perl -lne 'm{DB_NAME.*?([\w.-]+)} and print $1' $DIR/env.php)
-	perl -i -pwe "s|${DB_NAME}|wordpress_${WP_VERSION}|" $DIR/env.php
+	perl -i -pwe "s|${DB_NAME}|wordpress_${WP_NEW_VERSION}|" $DIR/env.php
 }
 
 install_wp() {
-	WP_PATH='--path=public/wp'
 	wp db create $WP_PATH > /dev/null 2>&1
 	if ! wp core is-installed $WP_PATH; then
-		wp core install $WP_PATH --url="http://wordpress.dev" --title="WordPress $WP_VERSION" --admin_user="dev" --admin_password="dev" --admin_email="dev@geminilabs.io"
+		echo "${BOLD}${GREEN}Installing WordPress $WP_NEW_VERSION${NORMAL}"
+		wp core install --url="http://wordpress.test" --title="WordPress $WP_NEW_VERSION" --admin_user="dev" --admin_password="dev" --admin_email="dev@geminilabs.io" $WP_PATH
 		wp option update welcome 0 $WP_PATH
 		wp option update uploads_use_yearmonth_folders 0 $WP_PATH
 		wp option update blogdescription "" $WP_PATH
@@ -79,61 +71,79 @@ install_wp() {
 		wp post delete 1 --force $WP_PATH
 		wp post update 2 --post_title=Home --post_name=home --post_content= $WP_PATH
 		wp user meta update 1 show_welcome_panel 0 $WP_PATH
-		wp widget deactivate $(wp widget list sidebar-1 --fields=id --format=ids $WP_PATH) $WP_PATH
 	fi
 }
 
 install_test_suite() {
+	echo "${BOLD}${GREEN}Installing Test Suite for WordPress $WP_NEW_VERSION${NORMAL}"
 	if [ ! -d $WP_TESTS_DIR ]; then
 		mkdir -p $WP_TESTS_DIR
-		svn co --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		svn co --quiet https://develop.svn.wordpress.org/tags/${WP_NEW_VERSION}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		svn co --quiet https://develop.svn.wordpress.org/tags/${WP_NEW_VERSION}/tests/phpunit/data/ $WP_TESTS_DIR/data
 	fi
-
 	rm -f $DIR/tests/current
 	ln -s $WP_TESTS_DIR $DIR/tests/current
-
 	DB_NAME=$(perl -lne 'm{DB_NAME.*?([\w.-]+)} and print $1' $DIR/env.php)
 	DB_USER=$(perl -lne 'm{DB_USER.*?([\w.-]+)} and print $1' $DIR/env.php)
 	DB_PASS=$(perl -lne 'm{DB_PASSWORD.*?([\w.-]+)} and print $1' $DIR/env.php)
 	DB_HOST=$(perl -lne 'm{DB_HOST.*?([\w.-]+)} and print $1' $DIR/env.php)
-
 	WP_TEST_CONFIG="$WP_TESTS_DIR/wp-tests-config.php"
-
 	if [ ! -f wp-tests-config.php ]; then
-		download https://develop.svn.wordpress.org/tags/${WP_VERSION}/wp-tests-config-sample.php $WP_TEST_CONFIG
+		download https://develop.svn.wordpress.org/tags/${WP_NEW_VERSION}/wp-tests-config-sample.php $WP_TEST_CONFIG
+		if [ ! -f "$WP_TEST_CONFIG" ]; then
+			echo "${RED}${BOLD}Failed to download wp-tests-config.php${NORMAL}"
+			exit 1
+		fi
 		perl -i -pwe "s|dirname.{22}|'${DIR}/public/wp/'|" $WP_TEST_CONFIG
 		perl -i -pwe "s|youremptytestdbnamehere|${DB_NAME}|" $WP_TEST_CONFIG
 		perl -i -pwe "s|yourusernamehere|${DB_USER}|" $WP_TEST_CONFIG
 		perl -i -pwe "s|yourpasswordhere|${DB_PASS}|" $WP_TEST_CONFIG
 		perl -i -pwe "s|localhost|${DB_HOST}|" $WP_TEST_CONFIG
 	fi
+}
 
-	if [ $(version $WP_VERSION) -lt $(version 4.5) ]; then
-		MYSQL_VERSION=`mysql -V | grep -Eo "\d+\.\d+\.\d+"`
-		# update the deprecated mysql storage_engine if version is greater than 5.5.2
-		if [ $(version $MYSQL_VERSION) -gt $(version 5.5.2) ]; then
-			perl -i -pwe "s|SET storage_engine |SET default_storage_engine |" "$WP_TESTS_DIR/includes/install.php"
-		fi
-	fi
+export_db() {
+	echo "${BOLD}${GREEN}Exporting Database${NORMAL}"
+	# TABLE_PREFIX=$(perl -lne 'm{table_prefix.*?([\w]+)} and print $1' $DIR/env.php)
+	# wp db export $DIR/db_$WP_PREVIOUS_VERSION.sql --add-drop-table --tables=$(wp db tables --all-tables-with-prefix --format=csv $WP_PATH) --quiet $WP_PATH
+	wp db export $DIR/db_$WP_PREVIOUS_VERSION.sql --add-drop-table --single-transaction --skip-set-charset --set-gtid-purged=OFF --quiet $WP_PATH
+}
+
+import_db() {
+	echo "${BOLD}${GREEN}Importing Database${NORMAL}"
+	mkdir -p $DIR/backups
+	# wp db export $DIR/backups/db_$WP_NEW_VERSION.sql $WP_PATH
+	# wp db export $DIR/backups/db_$WP_NEW_VERSION.sql --add-drop-table --tables=$(wp db tables --all-tables-with-prefix --format=csv $WP_PATH) $WP_PATH
+	wp db export $DIR/backups/db_$WP_NEW_VERSION.sql --add-drop-table --single-transaction --skip-set-charset --set-gtid-purged=OFF --quiet $WP_PATH
+	wp db import $DIR/db_$WP_PREVIOUS_VERSION.sql $WP_PATH
+	wp option update blogname "WordPress $WP_NEW_VERSION" --quiet $WP_PATH
+	wp core update-db --quiet $WP_PATH
+	rm $DIR/*.sql
 }
 
 check_errors
 
-read -e -p 'Enter the WordPress version to switch to (3.7 - 4.6): ' WP_VERSION
+read -e -p "You are using WordPress ${WP_PREVIOUS_VERSION}. Enter a new version to switch to (${WP_MIN_VERSION} - ${WP_MAX_VERSION}), or Enter for latest: " WP_NEW_VERSION
 
-REGEX_WP3='^3.([7-9]+)$'
-REGEX_WP4='^4.([0-6]+)$'
-
-if ! [[ $WP_VERSION =~ $REGEX_WP3 || $WP_VERSION =~ $REGEX_WP4 ]] ; then
-	WP_VERSION="4.6"
+if [[ -z "$WP_NEW_VERSION" ]] || [ $(echo "$WP_NEW_VERSION <= $WP_MIN_VERSION" | bc) -eq 1 ] || [ $(echo "$WP_NEW_VERSION >= $WP_MAX_VERSION" | bc) -eq 1 ]; then
+	WP_NEW_VERSION="$WP_MAX_VERSION"
 fi
 
+echo "${GREEN}Using: $WP_NEW_VERSION${NORMAL}"
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd)"
-WP_CORE_DIR="$DIR/versions/$WP_VERSION"
-WP_TESTS_DIR="$DIR/tests/$WP_VERSION"
+WP_CORE_DIR="$DIR/versions/$WP_NEW_VERSION"
+WP_TESTS_DIR="$DIR/tests/$WP_NEW_VERSION"
 
 cd $DIR
 
-switch_wp
-install_wp
-install_test_suite
+if ! [[ $WP_NEW_VERSION == $WP_PREVIOUS_VERSION ]]; then
+	export_db
+	switch_wp
+	install_wp
+	install_test_suite
+	import_db
+	echo "${GREEN}${BOLD}Switch complete.${NORMAL}"
+else
+	echo "${YELLOW}No version change needed.${NORMAL}"
+fi
